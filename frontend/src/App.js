fd876981acd2ac1c4c339566c4a96702f23ed9cb
omkarSw2 <omkar21143@gmail.com>
2023-09-26 22:46:51 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { HomePage } from "./pages/HomePage";
 function App() {
   return (
     <div className="App">
-      {/* <MainRoute /> */}
+      <MainRoute />
       <HomePage />
     </div>
   );
