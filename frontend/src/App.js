493ddf3f9ea1823c3f6c74fea7a76a7945f507d0
omkarSw2 <omkar21143@gmail.com>
2023-09-26 22:34:00 +0530

    Omkar_day_1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import MainRoute from "./Auth/MainRoute";
+import Footer from "./componant/Footer";
 
 function App() {
   return (
     <div className="App">
       <MainRoute />
+      <Footer />
     </div>
   );
 }
