c5ea4a49b2e55a68e6da866174d495cf3e6841d5
sachin kumar <sachinpatel.k.786@gmail.com>
2023-09-27 09:56:00 +0530

    USER ENDPOINTS CREATED

--- backend/db.js
+++ backend/db.js
@@ -1,2 +1,19 @@
-const mongoose = require("mongoose");
+
+const mongoose=require("mongoose")
 require("dotenv").config();
+
+const connection=mongoose.connect(process.env.MONGOURL)
+
+module.exports={
+    connection,
+}
+
+
+
+
+
+
+
+
+
+// 
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,24 @@
-const express = require("express");
+const express=require("express")
+const { connection } = require("mongoose")
+const { UserModel } = require("./model/user.model")
+const { userRouter } = require("./routes/userroutes")
+require("dotenv").config();
+const cors=require("cors")
+const app=express()
+app.use(express.json())
+app.use(cors())
+
+app.get("/",(req,res)=>{
+    res.send("hello")
+})
+app.use("/user",userRouter);
+
+app.listen(process.env.PORT,async()=>{
+    try {
+        await connection;
+        console.log("db connected")
+        console.log("server  is connected")
+    } catch (error) {
+        console.log(error)
+    }
+})
