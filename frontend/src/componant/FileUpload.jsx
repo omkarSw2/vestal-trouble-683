0fba7a2db07822669a4d9f0e25ab5bb40249d282
omkarSw2 <omkar21143@gmail.com>
2023-10-03 17:44:32 +0530

    upload is done

--- frontend/src/componant/FileUpload.jsx
+++ frontend/src/componant/FileUpload.jsx
@@ -1,102 +1,99 @@
-import { Input, Text } from '@chakra-ui/react';
-import axios from 'axios'
-import React, { useState } from 'react';
-import styled from 'styled-components'
-import { useToast } from '@chakra-ui/react'
+import { Input, Text } from "@chakra-ui/react";
+import axios from "axios";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useToast } from "@chakra-ui/react";
+import { useDispatch, useSelector } from "react-redux";
 
 function FileUploadForm() {
-
-
   const fileInput = React.createRef();
-  const toast = useToast()
+  const toast = useToast();
+  const data = useSelector((store) => store.authReducer);
+  const dispatch = useDispatch();
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
 
+    // const product = {
+    //     name: name,
+    //     price: price,
+    //     productImage: [fileInput.current.files[0].name, fileInput.current.files[1].name, fileInput.current.files[2].name, ],
+    // };
 
-     const onSubmit = async (e) => {
-      e.preventDefault();
+    // const data = new FormData()
 
-      // const product = {
-      //     name: name,
-      //     price: price,
-      //     productImage: [fileInput.current.files[0].name, fileInput.current.files[1].name, fileInput.current.files[2].name, ],
-      // };
-
-      // const data = new FormData()
-
-      const formData = new FormData();
+    const formData = new FormData();
     //   formData.append('OPOPO', name);
     //   formData.append('price', price);
-      formData.append('photos', fileInput.current.files[0]);
-      formData.append('photos', fileInput.current.files[1]);
-      formData.append('photos', fileInput.current.files[2]);
+    formData.append("photos", fileInput.current.files[0]);
+    // formData.append("photos", fileInput.current.files[1]);
+    // formData.append("photos", fileInput.current.files[2]);
 
-      console.log(fileInput.current.files[0].name);
-      console.log(fileInput.current.files[1].name);
-      console.log(fileInput.current.files[2].name);
+    console.log(fileInput.current.files[0].name);
+    // console.log(fileInput.current.files[1].name);
+    // console.log(fileInput.current.files[2].name);
 
-      
-
-
-      axios
-          .post("http://localhost:8080/addDieviant/uploads", formData)
-          .then((res) => {
-            console.log(res)
-            toast({
-              title: 'New Submission',
-              description: "Your Deviation ha been posted",
-              status: 'success',
-              duration: 2000,
-              isClosable: true,
-            })
-            fileInput.current.form.reset();
-          })
-          .catch((err) => console.log(err));
-
- 
+    const headers = {
+      Authorization: `${data.token}`,
+    };
+    axios
+      .post("http://localhost:8080/posts/create", formData, { headers })
+      .then((res) => {
+        console.log(res);
+        toast({
+          title: "New Submission",
+          description: "Your Deviation ha been posted",
+          status: "success",
+          duration: 2000,
+          isClosable: true,
+        });
+        fileInput.current.form.reset();
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
-      <DIV>
-          
-          <form onSubmit={onSubmit}>
-              
-              <div style={{}}>
-              
-              <div style={{ width: "25%", margin: "auto"}}>
-              <div style={{marginBottom: "20px"}}>
-                <Text mb={4}>Drag and drop your art here</Text>
-              <input type="file"  name='photos' ref={fileInput} multiple
-              style={{}}
-               />
-              </div>
-            
-             <div style={{}} >
-             <Input type="submit" value="Submit"  size='sm' 
-             className='Btn'
-             backgroundColor={'#63B3ED'}
-             
-             color={'white'}
-             fontSize={'20px'}
-             />
-             </div>
-              </div>
-              {/* <input type="submit" value="Submit" /> */}
-              </div>
-          </form>
+    <DIV>
+      <form onSubmit={onSubmit}>
+        <div style={{}}>
+          <div style={{ width: "25%", margin: "auto" }}>
+            <div style={{ marginBottom: "20px" }}>
+              <Text mb={4}>Drag and drop your art here</Text>
+              <input
+                type="file"
+                name="photos"
+                ref={fileInput}
+                multiple
+                style={{}}
+              />
+            </div>
 
-          <hr />
-      </DIV>
+            <div style={{}}>
+              <Input
+                type="submit"
+                value="Submit"
+                size="sm"
+                className="Btn"
+                backgroundColor={"#63B3ED"}
+                color={"white"}
+                fontSize={"20px"}
+              />
+            </div>
+          </div>
+          {/* <input type="submit" value="Submit" /> */}
+        </div>
+      </form>
+
+      <hr />
+    </DIV>
   );
 }
 
 export default FileUploadForm;
 
-
 const DIV = styled.div`
-
-  .Btn:hover{
+  .Btn:hover {
     cursor: pointer;
-    background-color: #3182CE
-
+    background-color: #3182ce;
   }
-`
+`;
