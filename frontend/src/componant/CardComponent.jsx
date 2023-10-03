c5417d3829290447d64e085a25a0ceb6aa91eadd
omkarSw2 <omkar21143@gmail.com>
2023-10-03 20:13:49 +0530

    omkar

--- frontend/src/componant/CardComponent.jsx
+++ frontend/src/componant/CardComponent.jsx
@@ -3,60 +3,62 @@ import React from "react";
 
 import { AiOutlineStar } from "react-icons/ai";
 import { FaRegCommentAlt } from "react-icons/fa";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-export const CardComponent = ({ image, user, userImage, comments, favourite, id }) => {
+export const CardComponent = ({
+  image,
+  user,
+  userImage,
+  comments,
+  favourite,
+  id,
+}) => {
   return (
     <Link to={`/singleProduct/${id}`}>
-    <div className='container'>
-     
-      <img className='contImage' src={image} alt='im' />
-      
+      <div className="container">
+        <img className="contImage" src={image} alt="im" />
 
-      <div className='bottom-left-user'>
-        {user}
-      </div>
+        <div className="bottom-left-user">{user}</div>
 
-      <div className='bottom-left'>
-        <Wrap>
-          <WrapItem>
-            <Avatar
-              size='xs'
-              name='Kola Tioluwani'
-              src={userImage}
-            />{' '}
-
-
-          </WrapItem>
-        </Wrap>
-        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginLeft: "5px" }}>
-          <div>
-            user nane
-          </div>
-        </div>
-
-        <div className="bottom-comment">
+        <div className="bottom-left">
+          <Wrap>
+            <WrapItem>
+              <Avatar size="xs" name="Kola Tioluwani" src={userImage} />{" "}
+            </WrapItem>
+          </Wrap>
           <div
             style={{
-              fontSize: "15px",
-              paddingBottom: "5px",
-              marginRight: "5px",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              marginLeft: "5px",
             }}>
-            {comments}
+            <div>user nane</div>
           </div>
-          <FaRegCommentAlt style={{ fontWeight: "bolder" }} />
-        </div>
 
-        <div className="star">
-          <div
-            style={{
-              fontSize: "15px",
-              paddingBottom: "0px",
-              marginRight: "5px",
-            }}>
-            {favourite}
+          <div className="bottom-comment">
+            <div
+              style={{
+                fontSize: "15px",
+                paddingBottom: "5px",
+                marginRight: "5px",
+              }}>
+              {comments}
+            </div>
+            <FaRegCommentAlt style={{ fontWeight: "bolder" }} />
           </div>
-          <AiOutlineStar style={{ fontSize: "20px", fontWeight: "bolder" }} />
+
+          <div className="star">
+            <div
+              style={{
+                fontSize: "15px",
+                paddingBottom: "0px",
+                marginRight: "5px",
+              }}>
+              {favourite}
+            </div>
+            <AiOutlineStar style={{ fontSize: "20px", fontWeight: "bolder" }} />
+          </div>
         </div>
       </div>
     </Link>
