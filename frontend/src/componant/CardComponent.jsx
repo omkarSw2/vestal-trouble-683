7e6a8df3b6e9f079acd89ad915b7585d005ad8be
omkarSw2 <omkar21143@gmail.com>
2023-10-03 20:43:34 +0530

    userId set

--- frontend/src/componant/CardComponent.jsx
+++ frontend/src/componant/CardComponent.jsx
@@ -5,41 +5,22 @@ import { AiOutlineStar } from "react-icons/ai";
 import { FaRegCommentAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-
 export const CardComponent = ({ postimage, userId, comments, _id, title }) => {
   return (
     <Link to={`/singleProduct/${_id}`}>
-    <div className='container'>
-     
-      <img className='contImage' src={postimage} alt={`${userId.first_Name}`} />
-      
+      <div className="container">
+        <img
+          className="contImage"
+          src={postimage}
+          alt={`${userId.first_Name}`}
+        />
 
-      <div className='bottom-left-user'>
-      {/* {title} */}
-      </div>
-
-      <div className='bottom-left'>
-        <Wrap>
-          <WrapItem>
-            <Avatar
-              size='xs'
-              name='Kola Tioluwani'
-              src={""}
-            />
-
-
-          </WrapItem>
-        </Wrap>
-        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginLeft: "5px" }}>
-          <div>
-          {`${userId.first_Name}`}
-          </div>
-        </div>
+        <div className="bottom-left-user">{/* {title} */}</div>
 
         <div className="bottom-left">
           <Wrap>
             <WrapItem>
-              <Avatar size="xs" name="Kola Tioluwani" src={userImage} />{" "}
+                <Avatar size="xs" name="Kola Tioluwani" src={""} />
             </WrapItem>
           </Wrap>
           <div
@@ -49,26 +30,43 @@ export const CardComponent = ({ postimage, userId, comments, _id, title }) => {
               alignItems: "center",
               marginLeft: "5px",
             }}>
-
-            {/* {comments} */}
+            <div>{`${userId.first_Name}`}</div>
           </div>
-          {/* <FaRegCommentAlt style={{ fontWeight: "bolder" }} /> */}
-        </div>
 
-        <div className="star">
-          <div
-            style={{
-              fontSize: "15px",
-              paddingBottom: "0px",
-              marginRight: "5px",
-            }}>
-            {/* {"gab"} */}
+          <div className="bottom-left">
+            <Wrap>
+              <WrapItem>
+                {/* <Avatar
+                  size="xs"
+                  name="Kola Tioluwani"
+                  src={"https://bit.ly/broken-link"}
+                />{" "} */}
+              </WrapItem>
+            </Wrap>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                marginLeft: "5px",
+              }}>
+              {/* {comments} */}
+            </div>
+            {/* <FaRegCommentAlt style={{ fontWeight: "bolder" }} /> */}
           </div>
-          {/* <AiOutlineStar style={{ fontSize: "20px", fontWeight: "bolder" }} /> */}
 
-          
+          <div className="star">
+            <div
+              style={{
+                fontSize: "15px",
+                paddingBottom: "0px",
+                marginRight: "5px",
+              }}>
+              {/* {"gab"} */}
+            </div>
+            {/* <AiOutlineStar style={{ fontSize: "20px", fontWeight: "bolder" }} /> */}
+          </div>
         </div>
-      </div>
       </div>
     </Link>
   );
